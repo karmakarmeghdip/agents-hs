e8834c306a26874a06925e7649a70f9bf8fb2dba
karmakarmeghdip <karmakarmeghdip@gmail.com>
2026-05-31 20:41:20 +0530

    feat: added devenv for nix support

---
+++ devenv.nix
@@ -0,0 +1,19 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  # https://devenv.sh/basics/
+  env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+  packages = [  ];
+
+  # https://devenv.sh/languages/
+  languages.haskell.enable = true;
+
+  # https://devenv.sh/tasks/
+  # tasks = {
+  #   "myproj:setup".exec = "mytool build";
+  #   "devenv:enterShell".after = [ "myproj:setup" ];
+  # };
+
+}
